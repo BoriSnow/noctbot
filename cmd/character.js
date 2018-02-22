8f871f16555354fdaf833baae5712e7740fc1cbe
BoriSnow <veragarf@gmail.com>
2018-02-21 21:45:48 -0500

    SQL works properly now.

--- cmd/character.js
+++ cmd/character.js
@@ -1,18 +1,23 @@
 const sql = require("sqlite");
 sql.open("./char.sqlite");
-exports.run = (bot, msg, [subcmd, mention, name, desc]) => {
+exports.run = (bot, msg, [subcmd, mention, name, ...desc]) => {
+  let info = desc.toString();
+  info = info.split(',').join(" ");
 if (subcmd == "list"){
 if (!name){
 return msg.reply("Please enter the name of the character!");
 }
 if (msg.mentions.members.size == 1) {
-  //find characters with that userID
+sql.get(`SELECT name, desc FROM chars WHERE userID = "${msg.mentions.members.first().id}"`);
 }
 if(name){
-  //find characters with that name
+sql.get(`SELECT userID, desc FROM chars WHERE name = "name"`);
 }
 if(name && msg.mentions.members.size == 1) {
-  //find that specific character
+sql.get(`SELECT name, desc FROM chars WHERE userID = "${msg.mentions.members.first().id}" AND name = "name"`).then(row => {
+  if(!row) return msg.reply("No such character!");
+msg.channel.send(`${msg.mentions.members.first()}'s Character: \n` + `${row.name}` + `\n` + `${row.desc}`);
+});
 }
 }
 else if (msg.member.roles.some(r=>["Bio Approver"].includes(r.name)) ){
@@ -20,8 +25,9 @@ else if (msg.member.roles.some(r=>["Bio Approver"].includes(r.name)) ){
   if(subcmd == "add"){
     if (!owner) return msg.reply("Please mention the owner of the character!");
     if (!name || !desc) return msg.reply("Please enter the name and details of the character!");
+    desc.join(" ");
     sql.get(`SELECT * FROM chars WHERE userID = "${msg.mentions.members.first().id}"`).then(row => {
-    sql.run("INSERT INTO chars (userID, name, desc) VALUES (?, ?, ?)", [msg.mentions.members.first().id, name, desc]);
+    sql.run("INSERT INTO chars (userID, name, desc) VALUES (?, ?, ?)", [msg.mentions.members.first().id, name, info]);
   }).catch(() =>
   {
     console.error;
@@ -29,22 +35,32 @@ else if (msg.member.roles.some(r=>["Bio Approver"].includes(r.name)) ){
   }
 /*****************************************************/
   else if(subcmd == "del"){
-    if (!name) return msg.reply("Please enter the character's name you wish to delete!");
-    if(msg.mentions.members.size !== 0 && name){
-      // delete that specific character
+    if(msg.mentions.members.size == 1 && !name){
+    sql.run(`DELETE FROM chars WHERE userID = "${msg.mentions.members.first().id}"`).catch(() =>
+  {
+    console.error;
+    console.log("Number of rows deleted: " + result.affectedRows);
+    return msg.reply("Characters successfully deleted.");
+  });
     }
-        if(msg.mentions.members.size !== 0){
-        sql.run(`DELETE FROM chars WHERE userID = "${msg.mentions.members.first().id}"`).catch(() =>
-      {
-        console.error;
-        console.log("Number of rows deleted: " + result.affectedRows);
-      });  // delete all of that person's characters
+else {
+return msg.reply("Please specify a user's characters to delete, or a specific character!!");
+  }
+    if(msg.mentions.members.size == 1 && name){
+      sql.get(`SELECT* FROM chars WHERE name = "name" AND userID = "${msg.mentions.users.first().id}"`).then(row => {
+        if(!row){
+        return msg.reply("There is no such character!");
         }
-  else {
-    sql.get(`SELECT* FROM chars WHERE name = mention"`).then(row)
-    //find and list owners and names that match it
-  }
-  }
+        else{
+          sql.run(`DELETE FROM chars WHERE userID = "${msg.mentions.members.first().id}" AND name = "name"`).catch(() =>
+        {
+          console.error;
+          console.log("Character successfuly deleted.");
+          return msg.reply("Character was successfully deleted.");
+        });
+        }
+    });
+  }}
   /***************************************************/
   else if(subcmd == "edit"){
   }
